0422f64465e23d0eda085105fed763aedb4e4f91
ω〆ƙεɳȿɧɩ ッ <124400130+WXKENSHI@users.noreply.github.com>
2024-05-27 00:06:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,96 @@
+function bodyLoad() 
+{
+  var typingEffect = new Typed(".author",
+  {
+    strings: ["تفصيلة","تفصيلة","تفصيلة"],
+    loop: true,
+    typeSpeed: 100,
+    backDelay: 1500,
+    backSpeed: 80,
+  })
+
+};
+
+i = 0;
+function navtoggle()
+{
+  if (i === 0)
+  {
+    var nav = document.getElementById('nav');
+    var pg = document.getElementById('page-content');
+    nav.style.visibility="visible";
+    nav.style.transform="translateX(200.5vw)";
+    // pg.style.filter="blur(3px)";
+    i++;
+  }
+  else if (i === 1)
+  {
+    var nav = document.getElementById('nav');
+    var pg = document.getElementById('page-content')
+    nav.style.transform="translateX(-200vw)";
+    nav.style.visibility="hidden";
+    // pg.style.filter="unset";
+    i--;
+    
+  }
+  
+}
+// close nav on page click
+function unnavpageclk()
+{
+  if (i === 1)
+  {
+    var nav = document.getElementById('nav');
+    var pg = document.getElementById('page-content')
+    nav.style.transform="translateX(-200vw)";
+    nav.style.visibility="hidden";
+    pg.style.filter="unset";
+    i--;
+  }
+  
+}
+
+function isTouchDevice() {
+  return (('ontouchstart' in window) ||
+     (navigator.maxTouchPoints > 0) ||
+     (navigator.msMaxTouchPoints > 0));
+}
+
+const touch = isTouchDevice();
+
+if (touch)
+  {
+    function anim()
+    {
+      var bartop = document.getElementById("bartop");
+      var barbottom = document.getElementById("barbottom");
+      bartop.style.transform="scaleX(1)";
+      barbottom.style.transform="scaleX(1)";
+    }
+
+    function unanim()
+    {
+      var bartop = document.getElementById("bartop");
+      var barbottom = document.getElementById("barbottom");
+      bartop.style.transform="scaleX(1)";
+      barbottom.style.transform="scaleX(1)";
+    }
+  }
+  else
+  {
+    function anim()
+    {
+      var bartop = document.getElementById("bartop");
+      var barbottom = document.getElementById("barbottom");
+      bartop.style.transform="scaleX(1.7)";
+      barbottom.style.transform="scaleX(1.99)";
+    }
+
+    function unanim()
+    {
+      var bartop = document.getElementById("bartop");
+      var barbottom = document.getElementById("barbottom");
+      bartop.style.transform="scaleX(1)";
+      barbottom.style.transform="scaleX(1)";
+    }
+  }
